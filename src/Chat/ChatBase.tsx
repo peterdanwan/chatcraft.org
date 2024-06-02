d3d6408a7cc915b09917fa256fa9f65b13c302d5
Amnish <amnishsingh04@gmail.com>
2024-06-02 19:41:28 -0400

    Add a stop announcing button for TTS

--- src/Chat/ChatBase.tsx
+++ src/Chat/ChatBase.tsx
@@ -18,6 +18,7 @@ import { ScrollRestoration } from "react-router-dom";
 import Header from "../components/Header";
 import MessagesView from "../components/MessagesView";
 import OptionsButton from "../components/OptionsButton";
+import PreferencesModal from "../components/Preferences/PreferencesModal";
 import PromptForm from "../components/PromptForm";
 import Sidebar from "../components/Sidebar";
 import { useAlert } from "../hooks/use-alert";
@@ -38,7 +39,8 @@ import {
 import { WebHandler } from "../lib/WebHandler";
 import { ChatCraftCommandRegistry } from "../lib/commands";
 import ChatHeader from "./ChatHeader";
-import PreferencesModal from "../components/Preferences/PreferencesModal";
+
+import DisableAudioPlayerButton from "../components/DisableAudioPlayerButton";
 
 type ChatBaseProps = {
   chat: ChatCraftChat;
@@ -59,7 +61,7 @@ function ChatBase({ chat }: ChatBaseProps) {
   const inputPromptRef = useRef<HTMLTextAreaElement>(null);
   const { error } = useAlert();
   const { user } = useUser();
-  const { clearAudioQueue } = useAudioPlayer();
+  const { clearAudioQueue, isPlaying } = useAudioPlayer();
   const [showAlert, setShowAlert] = useState(false);
   const {
     isOpen: isPrefModalOpen,
@@ -323,8 +325,10 @@ function ChatBase({ chat }: ChatBaseProps) {
 
         // NOTE: we strip out the ChatCraft App messages before sending to OpenAI.
         const messages = chat.messages({ includeAppMessages: false });
+
         // Clear any previous audio clips
         clearAudioQueue();
+
         const response = await callChatApi(messages, {
           functions,
           functionToCall,
@@ -407,7 +411,7 @@ function ChatBase({ chat }: ChatBaseProps) {
         </Alert>
       );
     }
-  }, [onPrefModalOpen, settings.currentProvider, showAlert]);
+  }, [onPrefModalOpen, showAlert]);
 
   return (
     <Grid
@@ -422,6 +426,8 @@ function ChatBase({ chat }: ChatBaseProps) {
       bgGradient="linear(to-b, white, gray.100)"
       _dark={{ bgGradient: "linear(to-b, gray.600, gray.700)" }}
     >
+      {isPlaying && <DisableAudioPlayerButton clearOnly={!streamingMessage} />}
+
       <GridItem colSpan={2}>
         {/* Default Provider Alert Banner*/}
         {defaultProviderAlert}
